28c06ad76e7d7ae27345a0bf40c7ca93056cfcd9
Michelle Wang <mwang@lendingclub.com>
2016-02-20 11:11:44 -0800

    first commit with basics setup

---
+++ scripts/wikipedia.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	
+	$("#wiki_summary").html("Shabby chic pinterest keytar hoodie blue bottle. Tacos fingerstache small batch, health goth disrupt waistcoat 8-bit franzen meggings vinyl lo-fi kogi. Mlkshk kickstarter poutine twee taxidermy, godard brooklyn offal forage. Synth kickstarter roof party, tilde messenger bag slow-carb XOXO man braid flexitarian 90's mumblecore irony affogato. Health goth VHS banh mi yr tumblr, tousled chia four dollar toast banjo narwhal post-ironic. Lo-fi master cleanse offal, kogi man braid tilde neutra street art swag fingerstache heirloom. You probably haven't heard of them polaroid offal, pork belly dreamcatcher +1 marfa brooklyn beard health goth church-key.");
+
+	$("a").attr("href", "https://en.wikipedia.org");
+})
